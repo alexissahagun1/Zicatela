ff2f219add04aff6b37a3db8464fc74d067e1a15
alexissahagun1 <alexissahagunitj7a@gmail.com>
2025-04-29 13:20:47 -0600

    vercel deploy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'source.unsplash.com'],
+    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
   },
+  // Configuración para Vercel
+  swcMinify: true,
+  poweredByHeader: false,
+  compress: true,
 };
 
 module.exports = nextConfig; 
